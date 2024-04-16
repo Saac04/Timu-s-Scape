cf9360a16b9b9ca8640542c5c2193e253fc279f4
Saac04 <santiagoaaguirrec@gmail.com>
2024-04-16 17:34:07 +0200

    Botones con codigo y titulo de juegp

---
+++ Timu's-escape-Unity/Assets/Scenes/PantallaDeInicio/PantallaDeInicio.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //SceneManager.LoadScene("SampleScene");
+
+        //Ambas hacen lo mismo
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+        Debug.Log("El jugador salio del juego");
+    }
+}
