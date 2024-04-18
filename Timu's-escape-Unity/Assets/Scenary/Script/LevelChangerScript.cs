cf9bd7e625ad31072e39197a038e435e74243e20
Diego Baldoví <diegobal007@gmail.com>
2024-04-18 11:14:06 +0200

    Transicion de nivel con fundidos de pantalla
    Al tocar el collider, se ejecuta un fundido blanco y cuando la pantalla esta totalmente blanca se carga la siguiente escena y hace un fade para ver la escena

--- Timu's-escape-Unity/Assets/Scenary/Script/LevelChangerScript.cs
+++ Timu's-escape-Unity/Assets/Scenary/Script/LevelChangerScript.cs
@@ -5,11 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class LevelChangerScript : MonoBehaviour
 {
+    public Animator animatorController;
+
+    void Start()
+    {
+        animatorController.Play("AnimationFadeInWhite");
+    }
+
     void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Colision");
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            Debug.Log("Me he activado");
+            animatorController.Play("AminationFadeOutWhite");
+            Invoke("ChangeToNextScene", 1f);
         }
+    }
+
+    void ChangeToNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
