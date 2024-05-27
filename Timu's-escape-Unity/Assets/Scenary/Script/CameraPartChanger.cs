c2d592a2fa55a2f4a6cc8e09fd72dea7928aa1b1
Diego Baldoví <diegobal007@gmail.com>
2024-05-27 11:09:40 +0200

    fondos de nivel añadidos

--- Timu's-escape-Unity/Assets/Scenary/Script/CameraPartChanger.cs
+++ Timu's-escape-Unity/Assets/Scenary/Script/CameraPartChanger.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class CameraPartChanger : MonoBehaviour
@@ -8,6 +9,10 @@ public class CameraPartChanger : MonoBehaviour
 
     public Transform positionUp;
     public Transform positionDown;
+
+    public Image backgroundImage;
+    public Material backgrounMaterialUp;
+    public Material backgroundMaterialDown;
 
     void Start()
     {
@@ -19,10 +24,12 @@ public class CameraPartChanger : MonoBehaviour
         // whereTo indica si la camara debe ir hacia arriba (1) o hacia abajo (0)
         if (whereTo == 1)
         {
+            backgroundImage.material = backgrounMaterialUp;
             CameraObj.transform.position = positionUp.position;
         } 
         else if(whereTo == 0)
         {
+            backgroundImage.material = backgroundMaterialDown;
             CameraObj.transform.position = positionDown.position;
         } else
         {
