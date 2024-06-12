3092d975f05ee27d79af53ac1a69f83f880c9973
Diego Baldoví <diegobal007@gmail.com>
2024-06-12 12:57:23 +0200

    particulas paraplataforma caida
    para cuando cae, se destruye y se recompone

--- Timu's-escape-Unity/Assets/Scenary/Script/PlataformaCaida.cs
+++ Timu's-escape-Unity/Assets/Scenary/Script/PlataformaCaida.cs
@@ -7,7 +7,7 @@ public class PlataformaCaida : MonoBehaviour
     public float fallWait = 1f;
     public float fallDistance = 2.5f;
     public float fallingspeed = 1f;
-    public float destroyWait = 0f;
+    public float destroyWait = 1f;
     public float reappearWait = 5f;
     private Vector3 originalPosition;
 
@@ -17,6 +17,8 @@ public class PlataformaCaida : MonoBehaviour
     private Coroutine reappearCoroutine;
     public AudioSource audioCaida;
     private bool isBreaking;
+
+    public ParticleSystem smokeParticles;
 
     public void Start()
     {
@@ -52,8 +54,12 @@ public class PlataformaCaida : MonoBehaviour
             yield return null;
         }
 
+        Debug.Log("Particulas");
+        smokeParticles.Play();
+        Debug.Log("Espero");
         yield return new WaitForSeconds(destroyWait);
 
+        Debug.Log("Escondo");
         HideChildren();
         reappearCoroutine = StartCoroutine(ReappearAfterDelay());
     }
@@ -102,6 +108,7 @@ public class PlataformaCaida : MonoBehaviour
         {
             child.SetActive(true);
         }
+        smokeParticles.Play();
     }
 }
 
